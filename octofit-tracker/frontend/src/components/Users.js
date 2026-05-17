08360d60d891a201eaf7d6324c32d3a6d71d0ca8
ashok-kum <167595587+ashok-kum@users.noreply.github.com>
2026-05-17 07:25:13 +0530

    Add explicit API endpoint comment to Users component

--- octofit-tracker/frontend/src/components/Users.js
+++ octofit-tracker/frontend/src/components/Users.js
@@ -4,6 +4,7 @@ import getApiBase from '../apiBase'
 export default function Users() {
   const [items, setItems] = useState([])
   const endpoint = 'users'
+  // API endpoint: https://$REACT_APP_CODESPACE_NAME-8000.app.github.dev/api/users/
 
   useEffect(() => {
     let mounted = true
